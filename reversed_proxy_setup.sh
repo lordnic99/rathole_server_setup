2644c310edd2fd9aeceaefaf879cdc61d41a5043
hoang11.nguyen <hoang11.nguyen@lge.com>
2024-04-16 16:42:10 +0900

    add logic for check if unzip not installed

--- reversed_proxy_setup.sh
+++ reversed_proxy_setup.sh
@@ -2,6 +2,26 @@
 
 read -p "How many tunnels you want to open (default is UDP): " TUNNEL_AMOUNT
 
+check_and_install_unzip() {
+  if ! command -v unzip &> /dev/null; then
+    echo "-> unzip is not installed. Installing..."
+
+    if [[ $(lsb_release -is) == "Ubuntu" || $(lsb_release -is) == "Debian" ]]; then
+      sudo apt-get update && sudo apt-get install unzip -y
+    elif [[ $(cat /etc/os-release | grep -w ID=centos) ]]; then
+      sudo yum install unzip -y 
+    elif [[ $(cat /etc/os-release | grep -w ID=fedora) ]]; then
+      sudo dnf install unzip -y
+    else
+      echo "-> Unsupported operating system. Please install unzip manually."
+      return 1
+    fi
+
+    echo "-> unzip installed successfully."
+  fi
+}
+
+
 download_with_progress() {
   local url="$1"
   local filename="$2"
@@ -18,7 +38,7 @@ download_with_progress() {
   fi
 }
 
-echo "[INFO] Start downloading rathole"
+echo "-> Start downloading rathole"
 
 # ----------- ratholde download logic ------------
 download_url="https://github.com/rapiz1/rathole/releases/latest/download/rathole-x86_64-unknown-linux-gnu.zip"
@@ -28,7 +48,8 @@ download_with_progress "$download_url" "$download_filename"
 
 # -------------------------------------------------
 
-echo "[INFO] Extracting rathole"
+echo "-> Extracting rathole"
+check_and_install_unzip()
 
 # -------------------------------------------------
 if [[ "${download_filename}" =~ \.zip$ ]]; then
@@ -43,6 +64,7 @@ fi
 
 chmod +x ./rathole
 
-# mv rathole /usr/local/bin/
+mv -f rathole /usr/local/bin/
+
 rm -f "$download_filename"  
 
